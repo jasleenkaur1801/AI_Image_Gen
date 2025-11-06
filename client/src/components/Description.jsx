06a9ad80749f308a8a6a0846114f38d31710762e
jasleenkaur1801 <jasskaur4m@gmail.com>
2025-11-07 00:11:46 +0530

    newcommit

---
+++ client/src/components/Description.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Description = () => {
+  return (
+    <div className='flex flex-col items-center justify-center my-24 p-6 md:px-28'>
+      <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>Create AI Images</h1>
+      <p className='text-gray-500 mb-8'>Tun your imagination into visuals</p>
+      <div>
+        <img src={assets.sample_img_1} alt="" className='w-80 xl:w-96 rounded-lg'/>
+      </div>
+      <div>
+        <h2>Hello</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Description

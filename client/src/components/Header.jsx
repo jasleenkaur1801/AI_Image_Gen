03be93f5cc4f9a6122f9c87f6683387231df03b1
jasleenkaur1801 <jasskaur4m@gmail.com>
2025-11-15 20:25:04 +0530

    backend

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {motion} from "framer-motion"
 import { assets } from '../assets/assets'
+import { AppContext } from '../context/AppContext'
+import {useNavigate} from 'react-router-dom'
 
 const Header = () => {
+
+    const {user,setShowLogin}=useContext(AppContext)
+    const navigate = useNavigate()
+
+    const onClickHandler = ()=>{
+        if(user){
+            navigate('/result')
+        }else{
+            setShowLogin(true)
+        }
+    }
+
   return (
     <motion.div className='flex flex-col justify-center items-center text-center my-20'
     initial={{opacity:0.2, y:100}}
@@ -28,7 +42,8 @@ const Header = () => {
         animate={{opacity:1,y:0}}
         transition={{delay:0.6,duration:0.8}}
         >Unleash your creativity with AI. Turn your imagination into visual art in seconds - just type, and watch the magic happen.</motion.p>
-        <motion.button className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full'
+        <motion.button onClick={onClickHandler}
+        className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full'
         whileHover={{scale: 1.05}}
         whileTap={{scale:0.95}}
         initial={{opacity:0}}
@@ -39,14 +54,24 @@ const Header = () => {
             Generate Images 
             <img className="h-6" src={assets.star_group} alt="" />
         </motion.button>
-        <div className='flex flex-wrap justify-center mt-16 gap-3'>
+        <motion.div 
+        initial={{opacity:0}}
+        animate={{opacity:1}}
+        transition={{delay:1,duration:1}}
+        className='flex flex-wrap justify-center mt-16 gap-3'>
             {Array(6).fill('').map((item,index)=>(
-                <img className='rounded hover:scale-105 transition-all duration-300 cursor-pointer max-sm:w-10' 
+                <motion.img 
+                whileHover={{scale:1.05,duration:0.1}}
+                className='rounded hover:scale-105 transition-all duration-300 cursor-pointer max-sm:w-10' 
                 src={index%2===0 ? assets.sample_img_2 : assets.sample_img_1}
                 alt="" key={index} width={70}/>
             ))}
-        </div>
-        <p className='mt-2 text-neutral-600'>Generated images from imagify</p>
+        </motion.div>
+        <motion.p 
+        initial={{opacity:0}}
+        animate={{opacity:1}}
+        transition={{delay:1.2,duration:0.8}}
+        className='mt-2 text-neutral-600'>Generated images from imagify</motion.p>
     </motion.div>
   )
 }

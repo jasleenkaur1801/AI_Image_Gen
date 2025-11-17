a566f76c64faea75e8f9fc6f6bbc1b835c7ed51c
jasleenkaur1801 <jasskaur4m@gmail.com>
2025-11-17 14:34:48 +0530

    backend going

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -31,17 +31,33 @@ const loginUser = async (req,res)=>{
         const user = await userModel.findOne({email})
 
         if(!user){
-            return res({success:false,message: 'User does not exist'})
+            return res.json({success:false,message: 'User does not exist'})
         }
         const isMatch = await bcrypt.compare(password,user.password)
         if(isMatch){
             const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
             res.json({success:true,token,user: {name: user.name}})
         }else{
-            return res({success:false,message: 'Invalid credentials'})
+            return res.json({success:false,message: 'Invalid credentials'})
         }
     }catch(error){
         console.log(error)
         res.json({success:false,message: error.message})
     }
 }
+
+const userCredits = async (req,res)=>{
+    try{
+        const {userId} = req.body
+
+        const user = await userModel.findById(userId)
+        res.json({success: true, credits: user.creditBalance, 
+            user:{name: user.name}})
+        
+    }catch(error){
+        console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
+}
+
+export {registerUser,loginUser,userCredits}

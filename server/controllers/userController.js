0638352faa2f7eb16c634b8c4649afebd4cef4f4
jasleenkaur1801 <jasskaur4m@gmail.com>
2025-11-20 22:08:37 +0530

    razorpay

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,9 @@
 import userModel from "../models/userModel.js";
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import razorpay from 'razorpay'
+import transactionModel from "../models/transactionModel.js";
+import { SchemaTypeOptions } from "mongoose";
 
 const registerUser = async (req,res)=>{
     try{
@@ -57,6 +60,59 @@ const userCredits = async (req,res)=>{
     }catch(error){
         console.log(error.message)
         res.json({success: false, message: error.message})
+    }
+}
+
+const razorpayInstance = new razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET
+});
+
+const paymentRazorpay = async(req,res)=>{
+    try{
+        const {userId,planId} = req.body
+        const userData = await userModel.findById(userId)
+        if(!userId || !planId){
+            return res.json({success: false,message: 'Missing details'})
+        }
+        
+        let credits, plan, amount, date
+        switch (planId) {
+            case 'Basic':
+                plan = 'Basic'
+                credits = 100
+                amount = 10
+                break;
+            case 'Advanced':
+                plan = 'Advanced'
+                credits = 500
+                amount = 50
+                break;
+            case 'Business':
+                plan = 'Business'
+                credits = 5000
+                amount = 250
+                break;
+            default:
+                return res.json({success: false,message: 'Plan not found'})
+        }
+        date = Date.now();
+        const transactionData = {
+            userId, plan, amount, credits, date
+        }
+        const newTransaction = await transactionModel.create(transactionData)
+
+        // const options: {
+        //     amount: amount*100,
+        //     currency: process.env.currency,
+        //     reciept: 
+        // }
+        // await razorpayInstance.orders.create(SchemaTypeOptions,(error,order)=>{
+
+        // })
+    }catch(error){
+        console.log(error)
+        res.json({success: false,message: error.message})
     }
 }
 

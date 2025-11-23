fef21d5406082874c5764cf582832fe827eec196
jasleenkaur1801 <jasskaur4m@gmail.com>
2025-11-23 21:56:10 +0530

    fee changes

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -9,7 +9,20 @@ const BuyCredit = () => {
   const {user,backendUrl, loadCreditsData, token, setShowLogin} = useContext(AppContext)
   const navigate = useNavigate()
   const initPay = async(order)=>{
-
+    const options = {
+      key: import.meta.env.VITE_RAZORPAY_KEY_ID,
+      amount: order.amount,
+      currency: order.currency,
+      name: 'Credits Payment',
+      description: 'Credits Payment',
+      order_id: order.id,
+      receipt: order.receipt,
+      handler: async (response)=>{
+        console.log(response);
+      }
+    }
+    const rzp = new window.Razorpay(options)
+    rzp.open()
   }
   const paymentRazorpay = async (planId)=>{
     try{
@@ -42,7 +55,7 @@ const BuyCredit = () => {
             <p className='text-sm'>{item.desc}</p>
             <p className='mt-6'>
             <span className='text-3xl font-medium'>${item.price}</span>/{item.credits} credits</p>
-            <button className='w-full bg-gray-800 text-white mt-8 text-sm rounded-md py-2.5 min-w-52'>{user ? 'Purchase' : 'Get Started'}</button>
+            <button onClick={()=>paymentRazorpay(item.id)} className='w-full bg-gray-800 text-white mt-8 text-sm rounded-md py-2.5 min-w-52'>{user ? 'Purchase' : 'Get Started'}</button>
           </div>
         ))}
       </div>
